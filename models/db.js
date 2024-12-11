4fa036e2771efe5faa77c7579405302c32b3e363
Sysuev <rpnkbest@gmail.com>
2024-12-11 13:08:40 +0200

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,10 +1,14 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'root',
-    database: process.env.DB_NAME || 'CourseProject'
+    host: process.env.DB_HOST || 'mysql-1ae538c1-rpnk-4dc1.l.aivencloud.com',
+    port: process.env.DB_PORT || 11402,
+    user: process.env.DB_USER || 'avnadmin',
+    password: process.env.DB_PASSWORD || 'AVNS_PMkTR92HdUD_gGsAQ1A',
+    database: process.env.DB_NAME || 'courseproject',
+    ssl: {
+        rejectUnauthorized: true
+    }
 });
 
 connection.connect(err => {
